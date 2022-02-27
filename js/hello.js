fac1b762f698825f6da9045ceb5792a38273e3aa
BbillahrariBBr <mb.billah8@gmail.com>
2022-02-27 10:53:19 +0600

    array

--- js/hello.js
+++ js/hello.js
@@ -1,4 +1,15 @@
-console.log(21);
-
 var arrayjs = [14,12,23,45,46]
+
+// add element at end
+arrayjs.push(87)
+console.log(arrayjs)
+//remove element from end
+arrayjs.pop()
+console.log(arrayjs)
+
+// add elemnet begining
+arrayjs.unshift(12)
+console.log(arrayjs)
+// remove element frm beging
+arrayjs.shift()
 console.log(arrayjs)
