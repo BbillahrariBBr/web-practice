af1f0cf54ff8f5fcfbf8a2938a614d7614db8e39
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-02 14:13:50 +0600

    fibonacci

---
+++ js/fiboittrative.js
@@ -0,0 +1,12 @@
+function fibnacci(n){
+    var fibo = [0,1];
+
+    for(var i = 2; i<=n; i++){
+        fibo[i] = fibo[i-1]+fibo[i-2];
+        
+    }
+    return fibo;
+}
+
+var result = fibnacci(12);
+console.log(result);
