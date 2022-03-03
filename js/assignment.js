dbfcb92b907cae1c92e11abb2907194cd5c3b5de
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-03 15:20:10 +0600

    Wood calculator complete

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,15 @@ function feetToMile(feet){
 
 var feetconvert = feetToMile(2112009);
 console.log(feetconvert);
+
+function woodCalculator(chair,table,bed){
+    var chairWood = 1*chair;
+    var tableWood = 3*table;
+    var bedWood = 5*bed;
+    var totalWood = chairWood + tableWood + bedWood;
+    return totalWood; 
+
+}
+
+var needFurniture = woodCalculator(14,5,12)
+console.log("total Wood need ", needFurniture, "Cubic feet");
