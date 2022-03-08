6412d287e634ec98eb4459078d74669d552f76fb
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-08 21:28:55 +0600

    functin using return

--- jsfaq/function.js
+++ jsfaq/function.js
@@ -1,23 +1,32 @@
 function evenify(num) {
+    let result
     if (num % 2 == 0) {
-        console.log(num,': is even');
+        result = num;
     }
     else{
-        console.log(num*2, ': is odd');
+        result = num*2;
     }
+    return result;
 }
+let even_double = evenify(17);
+console.log(even_double);
 
 function evenify_all(nums)  {
+    var even_arr = [];
     for (let i = 0; i < nums.length; i++) {
         const num = nums[i];
-        evenify(num);
+        result_even = evenify(num);
+        even_arr.push(result_even);
     }
+    return even_arr;
     
     
 }
 
 nums = [5,7,12,23,14,18];
-evenify_all(nums);
+let array_result = evenify_all(nums);
+console.log(array_result);
 
 ages = [13,15,17,20,34];
-evenify_all(ages);
+let age_result = evenify_all(ages);
+console.log(age_result);
