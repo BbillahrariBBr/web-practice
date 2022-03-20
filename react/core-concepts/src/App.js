a3450556d62f113d6b1ee15867eaf3fe27d0a487
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-20 20:51:54 +0600

    dynamic product

--- react/core-concepts/src/App.js
+++ react/core-concepts/src/App.js
@@ -2,19 +2,31 @@
 import './App.css';
 
 function App() {
+  const nayoks = ['razzaq', 'jasim', 'Alomgir', 'Salman', 'Shakib','shuvo','rubel']
   const products = [
     {name:'Photoshop', price:'$90.89'},
     {name:'Illustrator ', price:'$60.89'},
     {name:'Pdf Reader', price:'$23.89'},
-];
+]
+  const productNames = products.map(product => product.name)
+  console.log(productNames);
   return (
     <div className="App">
       <header className="App-header">
+        <p>I am a React person</p>
+        <ul>
+          {nayoks.map(nayok => <li>{nayok}</li>)}
+          {products.map(product => <li>{product.name}: {product.price}</li>)}
+        </ul>
+        {
+          products.map(product => <Product product={product}></Product>)
+        }
       <Person name="Abdullah" occupation="software Engineer"></Person>
       <Person name= "Bakibillah bin Firoz bin rashid" occupation= "Programmer"></Person>
-      <Product product = {products[0]}> </Product>
+      
+      {/* <Product product = {products[0]}> </Product>
       <Product product = {products[1]}> </Product>
-      <Product product = {products[2]}> </Product>
+      <Product product = {products[2]}> </Product> */}
 
 
       {/* <Product></Product> */}
