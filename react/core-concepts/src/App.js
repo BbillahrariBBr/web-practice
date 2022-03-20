433d73aca944069a587f8ddef355ad5a98970f76
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-20 13:36:00 +0600

    add product

--- react/core-concepts/src/App.js
+++ react/core-concepts/src/App.js
@@ -1,17 +1,46 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name:'Photoshop', price:'$90.89'},
+    {name:'Illustrator ', price:'$60.89'},
+    {name:'Pdf Reader', price:'$23.89'},
+];
   return (
     <div className="App">
       <header className="App-header">
       <Person name="Abdullah" occupation="software Engineer"></Person>
       <Person name= "Bakibillah bin Firoz bin rashid" occupation= "Programmer"></Person>
+      <Product product = {products[0]}> </Product>
+      <Product product = {products[1]}> </Product>
+      <Product product = {products[2]}> </Product>
+
+
+      {/* <Product></Product> */}
       </header>
-      
     </div>
   );
 }
+
+function Product(props) {
+  const productStyle={
+    border:'1px solid gray',
+    borderRadius:'10px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'}
+  const {name, price} = props.product;
+  return(
+    <div style={productStyle}>
+      <h2>{name}</h2>
+      <h5>{price}</h5>
+      <button>Buy now</button>
+
+    </div>
+  )
+};
 
 function Person(props) {
   const personStyle ={
