058781d80c8c205e686fcb9883457d3d1e2f559d
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-20 12:58:33 +0600

    same component dynamc data

--- react/core-concepts/src/App.js
+++ react/core-concepts/src/App.js
@@ -5,19 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Person></Person>
-      <Person></Person>
+      <Person name="Abdullah" occupation="software Engineer"></Person>
+      <Person name= "Bakibillah bin Firoz bin rashid" occupation= "Programmer"></Person>
       </header>
       
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
+  const personStyle ={
+    border:'2px solid red',
+    margin: '10px'
+  }
+  console.log(props);
   return(
-    <div>
-      <h1>Name: Bakibillah</h1>
-      <h3>Software Engineer</h3>
+    <div style={personStyle}>
+      <h1>Name: {props.name}</h1>
+      <h3>{props.occupation}</h3>
     </div>
   );
   
