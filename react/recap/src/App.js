9491a4e00c2d953e7f442b90c304e4e31edf82c8
BbillahrariBBr <mb.billah8@gmail.com>
2022-03-22 11:13:24 +0600

    reacap befor dynamic

--- react/recap/src/App.js
+++ react/recap/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const heros = ['Salaman', 'Shah Rukh', 'Amir', 'Dulkar','Mohon Lal']
+  const heros = [{name:'Salaman',prof:'business'},
+  {name: 'Shah Rukh', prof:'jobJibi'}, 
+  {name: 'Amir', prof:'trailor'}, {name:'Dulkar',prof:'archtech'},{name: 'Mohon Lal',prof:'killer'}]
   return (
     <div className="App">
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        {
+          heros.map(hero => <Person name= {hero.name} prof = {hero.prof}></Person>)
+        }
+
+        <MovieCounter></MovieCounter>
       <header className="App-header">
         
         <p>
@@ -23,6 +28,30 @@ function App() {
   );
 }
 
+function MovieCounter(){
+  const [count, setCount] = useState(5)
+  const handleClick = () => setCount(count +1);
+  const removeClick = () => setCount(count - 1)
+
+  return(
+    <div>
+      <button onClick={handleClick}>Add movie</button>
+      <h5>number of movie {count}</h5>
+      <MovieDisplay movies ={count+1}></MovieDisplay>
+      <button onClick={removeClick}>Remove movie</button>
+
+    </div>
+  )
+}
+
+function MovieDisplay(props) {
+  return(
+    <div>
+      <h4>Movies i have acted:{props.movies}</h4>
+    </div>
+  )
+}
+
 function Person(props){
   const personStyle ={
     border:'2px solid purple',
@@ -30,8 +59,8 @@ function Person(props){
   }
   return(
     <div style={personStyle}>
-      <h1> name : </h1>
-      <h3>profession:</h3>
+      <h1> name :{props.name} </h1>
+      <h3>profession:{props.prof}</h3>
     </div>
   )
 }
