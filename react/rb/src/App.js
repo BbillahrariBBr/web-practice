da5fcf16c26c88c0380e7ad830eb19190f9606a3
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-17 14:18:16 +0600

    first part end

--- react/rb/src/App.js
+++ react/rb/src/App.js
@@ -1,24 +1,14 @@
 
 import './App.css';
-import {Button, Card} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import News from './Component/News';
 
 function App() {
   return (
     <div>
       <h2>React Bootstrap</h2>
-      <Button variant="primary">Primary</Button>
-
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      
+      <News></News>
       
     </div>
   );
