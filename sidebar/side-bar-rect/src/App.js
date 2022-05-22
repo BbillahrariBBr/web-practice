d04448a44c4e802f0d75b8e21572e4a67299a5cd
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-22 12:16:21 +0600

    pages and route add

--- sidebar/side-bar-rect/src/App.js
+++ sidebar/side-bar-rect/src/App.js
@@ -1,9 +1,34 @@
 
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Dashboard from "./Pages/Dashboard";
+import Users from "./Pages/Users";
+import Anaytics from "./Pages/Anaytics";
+import FileManager from "./Pages/FileManager";
+import Messages from "./Pages/Messages";
+
+import Order from "./Pages/Order";
+import Saved from "./Pages/Saved";
+import Setting from "./Pages/Setting";
 
 function App() {
   return (
     <>
+    <Router>
+      <Routes>
+          <Route path='/' element={<Dashboard/>} ></Route>
+          <Route path='/users' element={<Users/>}/>
+          <Route path='/analytics' element={<Anaytics/>}/>
+          <Route path='/filemanager' element={<FileManager/>}/>
+          <Route path='/messages' element={<Messages/>}/>
+
+          <Route path='/order' element={<Order/>}/>
+          <Route path='/saved' element={<Saved/>}/>
+          <Route path='/setting' element={<Setting/>}/>
+          <Route path='*' element={<> Elemennt not found</>}></Route>
+        </Routes>
+    </Router>
     </>
   );
 }
---
+++ sidebar/side-bar-rect/src/Pages/Anaytics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Anaytics = () => {
+    return (
+        <div className='title'>
+            Analytics
+        </div>
+    );
+};
+
+export default Anaytics;
