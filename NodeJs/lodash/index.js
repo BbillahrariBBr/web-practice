cb5bdcbcbf581c069672997744ba1f2be9c52095
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-24 17:20:41 +0600

    node module

---
+++ NodeJs/lodash/index.js
@@ -0,0 +1,10 @@
+// setTimeout(() => {
+//     console.log('test');
+// }, 4000);
+// var a =5;
+// console.log(global.a);
+// console.log(__dirname);
+// console.log(__filename);
+
+const people = require('./people.js');
+console.log(people);
---
+++ NodeJs/lodash/people.js
@@ -0,0 +1,8 @@
+var people = ["sakib","tamim", "mashrafe"];
+var a =6;
+function test()
+{
+    console.log("test");
+}
+console.log(module);
+module.exports = people;
